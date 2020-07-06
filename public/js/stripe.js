18c0e8a8923c5dd3ceb16acf2f270283c2092ec7
christopherbclark <clark.ny@gmail.com>
2020-07-05 20:15:55 -0400

    Stripe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,14 +1,23 @@
 /* eslint-disable */
 import axios from 'axios';
+import { showAlert } from './alerts';
 const stripe = Stripe(
   'pk_test_51H0nYBJqiCKHuK6xPSI4jEKSzqDFxMNA12VigfL9qWbOVkON1xld1bnO13QckMgPUnS09Cjy67lBzUjcyTEeGuB100xVVzXqfD'
 );
 
 export const bookTour = async tourId => {
-  // 1) Get the checkout session from API response
-  const session = await axios(
-    `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
-  );
-  console.log(session);
-  // 2) Create checkout form + charge the credit card
+  try {
+    // 1) Get the checkout session from API response
+    const session = await axios(
+      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+    // 2) Create checkout form + charge the credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
 };
